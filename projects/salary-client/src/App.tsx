b63f7ab64a16b5624b001862c9516c8926a99d9d
tikkhun <s565338107@163.com>
2024-07-16 15:47:53 +0800

    chore: 完成了最基本的计算

--- projects/salary-client/src/App.tsx
+++ projects/salary-client/src/App.tsx
@@ -1,45 +1,93 @@
-import React from "react";
-import { createForm } from "@formily/core";
-import { FormProvider, FormConsumer, Field } from "@formily/react";
 import {
+  FormButtonGroup,
   FormItem,
   FormLayout,
   Input,
-  FormButtonGroup,
   Submit,
 } from "@formily/antd-v5";
+import { createForm } from "@formily/core";
+import { Field, FormConsumer, FormProvider } from "@formily/react";
+import { DEFAULT_OPTIONS, Salary } from "@tikkhun/salary-core";
+import { useState } from "react";
 
 const form = createForm();
-
 export default function App() {
+  const [result, setResult] = useState(new Salary(form.values).start());
+  function refreshResult() {
+    setResult(new Salary(form.values).start());
+  }
   return (
-    <FormProvider form={form}>
-      <FormLayout layout="vertical">
-        <Field
-          name="input"
-          title="Input box"
-          required
-          initialValue="Hello world"
-          decorator={[FormItem]}
-          component={[Input]}
-        />
-      </FormLayout>
-      <FormConsumer>
-        {() => (
-          <div
-            style={{
-              marginBottom: 20,
-              padding: 5,
-              border: "1px dashed #666",
-            }}
-          >
-            Real-time response：{form.values.input}
-          </div>
-        )}
-      </FormConsumer>
-      <FormButtonGroup>
-        <Submit onSubmit={console.log}>submit</Submit>
-      </FormButtonGroup>
-    </FormProvider>
+    <>
+      {JSON.stringify(form.values)}
+      <FormProvider form={form}>
+        <FormLayout layout="vertical">
+          <Field
+            name="salaryCycle"
+            title="薪资周期"
+            required
+            initialValue={DEFAULT_OPTIONS.salaryCycle}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+          <Field
+            name="salaryPerCycle"
+            title="周期内的薪水"
+            required
+            initialValue={DEFAULT_OPTIONS.salaryPerCycle}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+          <Field
+            name="workHoursPerDay"
+            title="每天工作几小时"
+            required
+            initialValue={DEFAULT_OPTIONS.workHoursPerDay}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+          <Field
+            name="publicVacation"
+            title="带薪假期"
+            required
+            initialValue={DEFAULT_OPTIONS.publicVacation}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+          <Field
+            name="workSystem"
+            title="工作制度"
+            required
+            initialValue={DEFAULT_OPTIONS.workSystem}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+          <Field
+            name="yearEndAward"
+            title="年终奖金"
+            required
+            initialValue={DEFAULT_OPTIONS.yearEndAward}
+            decorator={[FormItem]}
+            component={[Input]}
+          />
+        </FormLayout>
+        <FormConsumer>
+          {() => (
+            <div
+              style={{
+                marginBottom: 20,
+                padding: 5,
+                border: "1px dashed #666",
+              }}
+            >
+              {JSON.stringify(result, null, 2)}
+              {/* {result.start()} */}
+            </div>
+          )}
+        </FormConsumer>
+        <FormButtonGroup>
+          <Submit onSubmit={refreshResult}>submit</Submit>
+        </FormButtonGroup>
+      </FormProvider>
+    </>
   );
 }
